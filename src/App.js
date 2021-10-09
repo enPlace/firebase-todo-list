5c7593bcc76e046bd86e291bbcb484069f687313
enPlace <n.place@protonmail.com>
2021-10-09 13:15:23 -0300

    Set up firebase integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <small>
+          You are running this application in <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b>{" "}
+          mode.
+        </small>
+        <form>
+          <input
+            type="hidden"
+            defaultValue={process.env.REACT_APP_NOT_SECRET_CODE}
+          />
+        </form>
+      </div>
+      <div>
+        apiKey: <b> {process.env.REACT_APP_API_KEY}</b>, authDomain: {process.env.REACT_APP_AUTH_DOMAIN},
+        projectId: {process.env.REACT_APP_PROJECT_ID}, storageBucket:{" "}
+        {process.env.REACT_APP_STORAGE_BUCKET}, messagingSenderId:{" "}
+        {process.env.REACT_APP_MESSAGING_SENDER_ID}, appId: {process.env.REACT_APP_APP_ID},
+      </div>
     </div>
   );
 }
